5f2c261840a4fa6313710b73fe1ab292dc724375
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2023-07-06 17:54:46 +0200

    Updated about me section

--- src/About.jsx
+++ src/About.jsx
@@ -2,18 +2,35 @@ import React from 'react';
 import HomeImage from './assets/Bild3.png';
 
 function AboutSection() {
+    const handleAnchorClick = (textMessage) => {
+        alert(textMessage + ' profile not linked yet.');
+    };
+    
     return ( 
-        <section id="about"> {/* home */}
-            <div className="about-content"> {/* home-content */}
+        <section id="about">
+            <div className="about-content">
                 <h3>Hello! I'm</h3>
                 <h1>Ali Fadhel</h1>
                 <h3>And I'm a <span>Fullstack Developer</span></h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultrices sapien et orci ultricies, vel consequat purus ultrices.</p>
+                <p>
+                I have recently graduated from Malmö University with a bachelor's degree in Computer Science. During my studies, 
+                I focused on Java and JavaScript programming languages and gained a solid understanding of them. 
+                I have gained valuable experience in various aspects of software development, including Frontend and Backend development, 
+                as well as working with databases.
+                </p>
                 <div className="social-media">
-                    <a href="#"><i className='bx bxl-linkedin-square'></i></a>
-                    <a href="#"><i className='bx bxl-github'></i></a>
-                    <a href="#"><i className='bx bxl-twitter'></i></a>
-                    <a href="#"><i className='bx bxl-facebook'></i></a>
+                    <a href="https://www.linkedin.com/in/ali-fadhel-b17513177/" target="_blank">
+                        <i className='bx bxl-linkedin-square'></i>
+                    </a>
+                    <a href="https://github.com/AliAlbabily" target="_blank">
+                        <i className='bx bxl-github'></i>
+                    </a>
+                    <a href="#" onClick={() => handleAnchorClick("Twitter")}>
+                        <i className='bx bxl-twitter'></i>
+                    </a>
+                    <a href="#" onClick={() => handleAnchorClick("Facebook")}>
+                        <i className='bx bxl-facebook'></i>
+                    </a>
                 </div>
                 <a href="#" className="btn">Download CV</a>
             </div>
