106ac314e70f69b5be32b0b9af430a86a93246f2
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2023-07-30 21:42:29 +0200

    Few changes

--- src/Studies.jsx
+++ src/Studies.jsx
@@ -5,14 +5,14 @@ function StudiesSection() {
         <section id="studies">
             <div className="card">
                 <img src="https://idrottsforum.org/wp-content/uploads/2020/02/orkanen.jpg" alt="Image 1"/>
-                <h3>Systemutveckling - Malmö Universitet</h3>
-                <p>Utbildningen inom systemutveckling fokuserar på att lära studenter de nödvändiga kunskaperna och färdigheterna för att utveckla och implementera programvara och IT-lösningar. Den täcker områden som programmeringsspråk, systemanalys, databashantering, webbutveckling, mjukvaruarkitektur och projektledning. Genom praktisk träning och projektarbete får studenterna förståelse för hela utvecklingscykeln och hur man löser verkliga problem inom IT-branschen.</p>
+                <h3>Computer Systems Development - Malmö University</h3>
+                <p>The system development education focuses on teaching students the essential knowledge and skills to develop and implement software and IT solutions. It covers areas like programming languages, systems analysis, database management, web development, software architecture, and project management. Through practical training and project work, students gain insight into the entire development cycle and how to solve real IT industry problems.</p>
             </div>
 
             <div className="card">
                 <img src="https://scontent-cph2-1.xx.fbcdn.net/v/t31.18172-8/21949792_1667099169969784_5331230737187925255_o.jpg?_nc_cat=107&cb=99be929b-3346023f&ccb=1-7&_nc_sid=9267fe&_nc_ohc=s4aAfzYe26YAX_M6dr3&_nc_ht=scontent-cph2-1.xx&oh=00_AfBjgP5JEYq5VmxQ1HghrCPc9RLEcDRCKbKAizaLyunhoA&oe=64E88C6F" alt="Image 2"/>
-                <h3>Javautvecklare - EC Utbildning</h3>
-                <p>Utbildningen Javautvecklare har tillkommit för att förse näringslivet med personer som har spetskompetens inom programmeringsspråket Java. Målet med utbildningen är att de studerande efter utbildningen på ett professionellt sätt ska kunna arbeta med utveckling av Java och angränsade områden. Kunskap och praktisk erfarenhet från ledande ramverk och med moderna verktyg och metoder ingår i utbildningen.</p>
+                <h3>Java developer - EC Utbildning</h3>
+                <p>The Java developer training has been added to provide business with people who have cutting-edge skills in the Java programming language. The goal of the education is that the students after the education should be able to work in a professional way with the development of Java and related areas. Knowledge and practical experience from leading frameworks and with modern tools and methods are included in the education.</p>
             </div>
         </section>
     );
