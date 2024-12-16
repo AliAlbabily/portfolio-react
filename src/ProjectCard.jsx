3908c7ae7cff46d2e44a9f126e3344c5c2103342
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2024-12-16 23:56:13 +0100

    Skills collapse functionality

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, CardActions } from '@mui/material';
+import { Button, CardActions, Collapse, Box } from '@mui/material';
 
 function ProjectCard({name, imageUrl, githubUrl, demoIsAvailable, demoUrl}) {
+    const [skillsExpanded, setSkillsExpanded] = useState(false);
 
     const handleGithubButtonClick = () => {        
         window.open(githubUrl, '_blank', 'noopener,noreferrer')
@@ -13,6 +14,10 @@ function ProjectCard({name, imageUrl, githubUrl, demoIsAvailable, demoUrl}) {
 
     const handleDemoButtonClick = () => {        
         window.open(demoUrl, '_blank', 'noopener,noreferrer')
+    };
+
+    const handleSkillsButtonClick = () => {
+        setSkillsExpanded(!skillsExpanded);
     };
 
     return ( 
@@ -29,8 +34,18 @@ function ProjectCard({name, imageUrl, githubUrl, demoIsAvailable, demoUrl}) {
             <CardActions>
                 <Button size="small" variant="contained" onClick={handleGithubButtonClick}>Github</Button>
                 {demoIsAvailable ? <Button size="small" variant="contained" onClick={handleDemoButtonClick}>Demo</Button> : null}
-                <Button size="small" variant="outlined">Skills</Button>
+                <Button size="small" variant="outlined" onClick={handleSkillsButtonClick}>Skills</Button>
             </CardActions>
+            <Collapse in={skillsExpanded} timeout="auto" unmountOnExit>
+                <Box sx={{ padding: 2 }}>
+                <Typography variant="body2" color="text.secondary">
+                    • JavaScript <br />
+                    • React <br />
+                    • Material UI <br />
+                    • Node.js <br />
+                </Typography>
+                </Box>
+            </Collapse>
         </Card>
     );
 }
